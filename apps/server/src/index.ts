72860954219e4fe0997829097303600181f95369
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2023-07-11 10:13:34 +0700

    cleanup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,7 +6,7 @@ import { verifyCupSlug, verifyEntityId, verifyResourceName } from './utils'
 
 export const app = new Elysia()
   .use(cors({ origin: '*' }))
-  .get('/', () => JSON.stringify({
+  .all('/', () => JSON.stringify({
     success: true,
     code: 201,
     message: 'Welcome!',
@@ -15,43 +15,28 @@ export const app = new Elysia()
   }))
   .group('/:cupId', {
     beforeHandle: async ({ set, params: { cupId } }) => verifyCupSlug(cupId),
-    params: t.Object({
-      cupId: t.String()
-    })
   }, (cupGroup) => cupGroup
-    .get('', async ({ params: { cupId } }) => await getCupFormSlug(cupId))
-    .delete('', async ({ params: { cupId } }) => await deleteCup(cupId))
-  )
-
-app.group('/:cupId/:resourceId', {
-  beforeHandle: ({ params: { resourceId } }) => verifyResourceName(resourceId),
-  params: t.Object({
-    cupId: t.String(),
-    resourceId: t.String()
+    .get('/', async ({ params: { cupId } }) => await getCupFormSlug(cupId))
+    .delete('/', async ({ params: { cupId } }) => await deleteCup(cupId))
+    .group('/:resourceId', {
+      beforeHandle: ({ params: { resourceId } }) => console.log("ResourceId", resourceId),
+    }, (resourceGroup) => resourceGroup
+      .get('/', async ({ params: { cupId, resourceId } }) => await getEntities(cupId, resourceId))
+      .post('/', async ({ params: { cupId, resourceId }, body }) => await createResourceEntity(cupId, resourceId, body))
+      .delete('/', async ({ params: { cupId, resourceId } }) => await deleteResource(cupId, resourceId))
+      .group('/:entityId', {
+        beforeHandle: ({ params: { entityId } }) => console.log("EntityId", entityId),
+      }, (entityGroup) => entityGroup
+        .get('/', async ({ params: { cupId, resourceId, entityId } }) => await getEntity(cupId, resourceId, entityId))
+        .put('/', async ({ params: { cupId, resourceId, entityId }, body }) => await updateEntity(cupId, resourceId, entityId, body))
+        .delete('/', async ({ params: { cupId, resourceId, entityId } }) => await deleteEntity(cupId, resourceId, entityId))
+      )
+    )
+  ).listen({
+    port: process.env.PORT || 3000,
+    hostname: process.env.HOSTNAME || 'localhost'
   })
-}, (resourceGroup) => resourceGroup
-  .post('', async ({ params: { cupId, resourceId }, body }) => await createResourceEntity(cupId, resourceId, body))
-  .get('', async ({ params: { cupId, resourceId } }) => await getEntities(cupId, resourceId))
-  .delete('', async ({ params: { cupId, resourceId } }) => await deleteResource(cupId, resourceId))
-)
 
-app.group('/:cupId/:resourceId/:entityId', {
-  beforeHandle: ({ params: { entityId } }) => verifyEntityId(entityId),
-  params: t.Object({
-    cupId: t.String(),
-    resourceId: t.String(),
-    entityId: t.String(),
-  })
-}, (entityGroup) => entityGroup
-  .get('', async ({ params: { cupId, resourceId, entityId } }) => await getEntity(cupId, resourceId, entityId))
-  .put('', async ({ params: { cupId, resourceId, entityId }, body }) => await updateEntity(cupId, resourceId, entityId, body))
-  .delete('', async ({ params: { cupId, resourceId, entityId } }) => await deleteEntity(cupId, resourceId, entityId))
-)
-
-app.listen({
-  port: process.env.PORT || 3000,
-  hostname: process.env.HOSTNAME || 'localhost'
-})
 console.log(`⚡ CRUDCup is running at ${app.server?.hostname}:${app.server?.port}`)
 
 purgeExpiredCups()
